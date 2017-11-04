dd92ba07ee6e9e62443941a4c90c7a2c758502e6
Yamuna Navada <yamunanavada@gmail.com>
2017-11-04 15:43:48 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,6 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
@@ -12,5 +12,3 @@ def happy_birthday(birthday_kids)
     end
   end
 end
-
-
